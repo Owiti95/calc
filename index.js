d7c738d843be56e9c5f1e8604e90c7f1dffd55aa
hannington owiti <hanningtonowiti@gmail.com>
2025-03-16 17:23:59 +0300

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
-let result = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el")
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
@@ -11,21 +11,21 @@ let result = document.getElementById("sum-el")
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 function add(){
-    let sum = num1 + num2
-    result.textContent = "Sum: " + sum
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
 }
 
 function subtract(){
-    let subtracter = num1 - num2
-    result.textContent = "Subtraction: " + subtracter
+    let result = num1 - num2
+    sumEl.textContent = "Subtraction: " + result
 }
 
 function divide(){
-    let div = num1/num2
-    result.textContent = "Division: " + div
+    let result = num1/num2
+    sumEl.textContent = "Division: " + result
 }
 
 function multiply(){
-    let mult = num1 * num2
-    result.textContent = "Multiplication: " + mult
+    let result = num1 * num2
+    sumEl.textContent = "Multiplication: " + result
 }
